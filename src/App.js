ecef88833b6bbdcb29df36d43ee30d9e3f51ffdc
Abi Mirza <amirza@techlaunch.io>
2021-03-24 01:37:58 -0400

    Change Task Reminder and delete Task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import { useState } from 'react';
+import Task from './components/Task';
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id:1,
+            text:'Food Shopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder:true
+        },
+        {
+            id:2,
+            text:'Cloth Shopping',
+            day: 'Feb 6th at 2:30pm',
+            reminder:false
+        },
+        {
+            id:3,
+            text:'Groceries Shopping',
+            day: 'Feb 7th at 2:30pm',
+            reminder:true
+        },
+    ]
+)
+
+// Delete Task
+const deleteTask = (id) =>{
+  setTasks(tasks.filter((task)=> task.id !== id))
+}
+
+// Toggle reminder
+
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task)=>
+      task.id === id? {
+        ...task,
+        reminder: !task.reminder,
+      } : task
+    )
+  )
+}
+
   return (
     <div className="container">
       <Header title="Task Manager"/>
-      <Tasks/>
+      {tasks.length>0? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> ) : ( 'No Task To Show')
+      }
+      
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,33 +1,12 @@
-import { useState } from 'react';
+import Task from './Task'
 
-const Tasks = () =>{
-    const [tasks, setTasks] = useState(
-        [
-            {
-                id:1,
-                text:'Food Shopping',
-                day: 'Feb 5th at 2:30pm',
-                reminder:true
-            },
-            {
-                id:2,
-                text:'cloth Shopping',
-                day: 'Feb 6th at 2:30pm',
-                reminder:false
-            },
-            {
-                id:3,
-                text:'Groceries Shopping',
-                day: 'Feb 7th at 2:30pm',
-                reminder:true
-            },
-        ]
-    )
+const Tasks = ({tasks, onDelete, onToggle}) =>{
+
     return (
         <>
-            {tasks.map((task) => {
-                <h3 key={task.id}>{task.text}</h3>
-            })}
+            {tasks.map((task) => (
+                <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>
+            ))}
         </>
     )
 }
